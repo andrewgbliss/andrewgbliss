446f514fe2e8decb4310060d6d4b6fac106ce59b
Andrew Bliss <andrewgbliss@gmail.com>
2023-06-08 18:51:19 -0600

    swapi

--- src/pages/swapi.tsx
+++ src/pages/swapi.tsx
@@ -1,13 +1,5 @@
-import type { NextPage } from 'next';
-// import { useQuery } from 'react-query';
-import { useState, useMemo, useEffect } from 'react';
-
-// Show a table of swapi people. This table will be paginated.
-// Fetch the people from swapi
-// Show / Map over results and create the table
-// Show a prev / next button to handle pagination
-// Keep track of what page your on
-// Basic styling
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { useState, useMemo } from "react";
 
 type SwapiResponse = {
   count: number;
@@ -22,44 +14,28 @@ type Person = {
   birth_year: string;
 };
 
-const Swapi: NextPage = () => {
+const Swapi = () => {
   const [page, setPage] = useState<number>(1);
-  const [data] = useState<SwapiResponse>();
-  const [isFetching, setIsFetching] = useState<boolean>(false);
 
-  // const { data, error, isLoading, isFetching } = useQuery<SwapiResponse>(
-  //   ['people', page],
-  //   () => fetchPage(page),
-  //   { keepPreviousData: true }
-  // );
-
-  useEffect(() => {
-    // let cancel = false;
-    const fetchPage = () => {
-      setIsFetching(true);
-      //const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
-      // const results = await res.json();
-      // if (!cancel) {
-      //   setData(results);
-      // }
-      setIsFetching(false);
-    };
-    fetchPage();
-    return () => {
-      // cancel = true;
-    };
-  }, [page]);
+  const { data, error, isLoading, isFetching } = useQuery<SwapiResponse>(
+    ["people", page],
+    () =>
+      fetch(`https://swapi.dev/api/people/?page=${page}`).then((res) =>
+        res.json()
+      ),
+    { keepPreviousData: true }
+  );
 
   const pageCount = useMemo(() => {
     if (!data?.count) return 1;
     return Math.ceil(data?.count / 10);
   }, [data?.count]);
 
-  // if (isLoading) return <div>Loading ...</div>;
-  // if (error) return <div>Error ...</div>;
+  if (isLoading) return <div>Loading ...</div>;
+  if (error) return <div>Error ...</div>;
 
   return (
-    <div className="flex flex-col gap-2 items-center font-mono">
+    <div className="flex flex-col items-center gap-2 font-mono">
       <div className="h-32" />
       <h1 className="text-2xl">Swapi People</h1>
       <h5>Page: {page}</h5>
@@ -93,7 +69,7 @@ const Swapi: NextPage = () => {
           >
             Prev
           </button>
-          {isFetching ? <div>Loading...</div> : ''}
+          {isFetching ? <div>Loading...</div> : ""}
           <button
             className="btn btn-primary"
             onClick={() => setPage(page + 1)}
@@ -107,4 +83,12 @@ const Swapi: NextPage = () => {
   );
 };
 
-export default Swapi;
+const queryClient = new QueryClient();
+
+export default function SwapiPage() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Swapi />
+    </QueryClientProvider>
+  );
+}
